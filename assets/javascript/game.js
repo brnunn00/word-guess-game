4a85d8370f2f174372c4de27bedc22f459117966
BryGuy <brnunn00@gmail.com>
2019-03-09 08:47:41 -0800

    working game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,17 @@
-document.addEventListener("DOMContentLoaded", function(event) {
 
 
 var guessesRemaining = 5;
-var lettersRemaining  =0 ;
+var lettersRemaining = 0;
 var gameStarted = false;
 var wordToGuess = "";
 var letterObj;
 var score = 0;
 var lettersPicked = [];
-var validPicked  = [];
+var validPicked = [];
 var answer = [];
 var answerKey = [];
+var remaniningArr = [];
 
-var wordBank = ["Hot Dogs", "Popcorn", "Nachos", "Beer", "wine", "garlic fries", "a foul ball"];
 
 
 var letterPicked = '';
@@ -32,48 +31,36 @@ document.onkeyup = function () {
         processChoice(letterPicked, answer, answerKey);
       }
     }
-  } else if (document.readyState == "complete" && gameStarted == false){
-    
-    gameStarted = true;
-    guessesRemaining = 5;
-    var x = document.getElementById("wordToGuess");
-     document.getElementById("prompt").textContent = "Hint: Things you can get at a baseball game";
-     document.getElementById("lettersPicked").textContent = "";
-     document.getElementById("guessesRemaining").textContent = guessesRemaining;
-    wordToGuess = getComputerChoice();
-    letterObj = buildBoard(wordToGuess, answer);
-    x.innerHTML = letterObj.board;
-    answerKey = letterObj.validLetters;
-    answer = letterObj.fullAnswer;
-   lettersPicked = [];
-validPicked  = [];
+  } else if (document.readyState == "complete" && gameStarted == false) {
 
+    newGame();
+  
   }
 }
-
-
-
-
-
-
 function processChoice(key, answer, valid) {
   if (valid.indexOf(key) > -1) {
-    rebuildBoard(key, answer);
+
     validPicked.push(key);
+    // lettersRemaining--;
     lettersPicked.push(key);
+    lettersRemaining = (answerKey.length - validPicked.length) 
+    rebuildBoard(validPicked);
   } else {
     document.getElementById("lettersPicked").textContent += " " + key;
     lettersPicked.push(key);
     guessesRemaining--;
     document.getElementById("guessesRemaining").textContent = guessesRemaining;
-    checkForGameOver(guessesRemaining);
-  }
 
+  }
+  checkForGameOver(guessesRemaining, lettersRemaining);
 }
 
 
 function getComputerChoice() {
-  var randomSelectedNumber = Math.random() * wordBank.length;
+  //Don't look at this, it's cheating!
+var wordBank= ["Hot Dogs", "Popcorn", "Nachos", "Beer", "wine", "garlic fries", "a foul ball"];
+  
+var randomSelectedNumber = Math.random() * wordBank.length;
   var randomIndex = Math.floor(randomSelectedNumber);
   var word = wordBank[randomIndex];
   word = word.toLowerCase();
@@ -82,50 +69,85 @@ function getComputerChoice() {
   return word;
 }
 
-function buildBoard(compWord, letterBank) {
+function buildBoard(compWord) {
 
-  var arr = compWord.split(" ");
-var answerBoard = [];
+
   var strB = '';
   var larr = [];
   var fullArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    var word = arr[i];
-    for (let j = 0; j < word.length; j++) {
-      strB += "_ ";
-      var char = word.charAt(j);
-      if (larr.indexOf(char == -1)) {
-        larr.push(char);
-      }
-      fullArr.push(char);
-      answerBoard.push("_ ");
-    }
-    strB += "&nbsp &nbsp &nbsp";
-    answerBoard.push("   ");
-  }
+  var lettersTotal =0;
 
+  for (let i = 0; i < compWord.length; i++) {
+    const curLetter = compWord[i];
+    if (curLetter === ' ') {
+      strB += "&nbsp";
+    } else {
+      strB += "_ ";
+      console.log(larr.indexOf(curLetter.toString() === -1));
+      if (!larr.includes(curLetter)) {
+        larr.push(curLetter.toString());
+        console.log(larr);
+      }
+    }
+    fullArr.push(curLetter);
+
+  }
   var obj = {};
   obj.board = strB;
   obj.validLetters = larr;
+  obj.letterCount = larr.length;
   obj.fullAnswer = fullArr;
   return obj;
 }
 
-function rebuildBoard(key, answer) {
-
+function rebuildBoard(correctLetters) {
+  var boardStr = '';
   for (let z = 0; z < answer.length; z++) {
-    const element = answer[z];
+    if (answer[z] == " ") {
+      boardStr += " ";
+    } else if (correctLetters.indexOf(answer[z]) > -1) {
+      boardStr += answer[z];
+    } else {
+      boardStr += "_ ";
+    }
 
   }
+  document.getElementById("wordToGuess").textContent = boardStr;
+  // document.getElementById("lettersRemaining").textContent = lettersRemaining;
+
 
 }
 
-function checkForGameOver(guesses,remains){
-  if (guesses==0){
+function checkForGameOver(guesses, remaining) {
+  if (guesses == 0) {
     alert("Game Over Man");
     document.getElementById("prompt").innerText = "GAME OVER! Press any key to load a new word";
     gameStarted = false;
-
+    return true;
+  } else if (remaining === 0){
+    alert("You Win!");
+    document.getElementById("prompt").innerText = "Good Job! Press any key to load a new word";
+    gameStarted = false;
+    return true;
   }
+  //put in check for letters remaining here. 
 }
-});
+
+function newGame(){
+  gameStarted = true;
+  guessesRemaining = 5;
+  var x = document.getElementById("wordToGuess");
+  document.getElementById("prompt").textContent = "Hint: Things you can get at a baseball game";
+  document.getElementById("lettersPicked").textContent = "";
+  document.getElementById("guessesRemaining").textContent = guessesRemaining;
+   wordToGuess = getComputerChoice();
+  letterObj = buildBoard(wordToGuess, answer);
+  x.innerHTML = letterObj.board;
+  answerKey = letterObj.validLetters;
+  answer = letterObj.fullAnswer;
+  
+  lettersRemaining = letterObj.letterCount;
+  // document.getElementById("lettersRemaining").textContent = lettersRemaining;
+  lettersPicked = [];
+  validPicked = [];
+}
